20830ba040510115db90f3276b66cd6f7fd7799f
Valiev T.M <cptchunk@yandex.ru>
2022-10-30 22:28:01 +0300

    add admin panel and models

--- src/someapp/admin.py
+++ src/someapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from someapp.models import Album, Track
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'artist']
+    
+    
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ['album', 'order', 'title', 'duration']
+
+
--- src/someapp/models.py
+++ src/someapp/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
-class Ttest(models.Model):
-    name = models.TextField()
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+
+class Track(models.Model):
+    album = models.ForeignKey(Album, related_name='tracks', on_delete=models.CASCADE)
+    order = models.IntegerField()
+    title = models.CharField(max_length=100)
+    duration = models.IntegerField()
+
+    class Meta:
+        unique_together = ['album', 'order']
+        ordering = ['order']
+
+    def __str__(self):
+        return '%d: %s' % (self.order, self.title)
